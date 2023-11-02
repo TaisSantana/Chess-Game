65b5ea7fb4cf2bc43c51f8fa9334bdd2f37f1cb8
TaisSantana <tais_santana002@hotmail.com>
2023-11-02 18:10:02 -0300

    classes básicas jogo de xadrez e teste do print do tabuleiro

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import chess.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Jogo de Xadrez!");
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,53 @@
+package chess;
+
+import boardgame.Board;
+
+public class ChessMatch {
+    private int turn;
+    private Color currentPlayer;
+    private Board board;
+    private boolean check;
+    private boolean checkMate;
+    private ChessPiece enPassantVulnerable;
+    private ChessPiece promoted;
+
+    public ChessMatch() {
+        board = new Board(8, 8);
+        //turn = 1;
+        //currentPlayer = Color.WHITE;
+        //initialSetup();
+    }
+    public int getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public boolean getCheck() {
+        return check;
+    }
+
+    public boolean getCheckMate() {
+        return checkMate;
+    }
+
+    public ChessPiece getEnPassantVulnerable() {
+        return enPassantVulnerable;
+    }
+
+    public ChessPiece getPromoted() {
+        return promoted;
+    }
+
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+        for (int i=0; i<board.getRows(); i++) {
+            for (int j=0; j<board.getColumns(); j++) {
+                mat[i][j] = (ChessPiece) board.piece(i, j);
+            }
+        }
+        return mat;
+    }
+}
