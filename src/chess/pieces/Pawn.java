9197f7846915b19930d29e08c0779344cffdb4dc
TaisSantana <tais_santana002@hotmail.com>
2023-11-02 21:56:21 -0300

    criando todas as peças e adicionando exception do chess

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,17 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece {
+    public Pawn(Board board, Color color, ChessMatch chessMatch) {
+        super(board,color);
+    }
+
+    @Override
+    public String toString() {
+        return "P";
+    }
+}
